f3e252b6b094d6d87a31f6a9b3446bad0d6acef4
Sven <nebel.sven@gmail.com>
2017-04-01 08:59:50 +0100

    Just some more exploratory code

--- tailf.py
+++ tailf.py
@@ -1,24 +1,26 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+
 # https://github.com/karelzak/util-linux/blob/master/text-utils/tailf.c
 # http://stackoverflow.com/questions/136168/get-last-n-lines-of-a-file-with-python-similar-to-tail
 # http://stackoverflow.com/questions/32901504/reading-lines-backwards-relative-to-a-line-in-python
 # https://github.com/kasun/python-tail/blob/master/tail.py
 # http://www.manugarg.com/2007/04/real-tailing-in-python.html
 
-# Read last n lines
-# Watch for changes on the file and print them (inotify vs rollfile)
-# Apply callback function to every line as improvement
-# What happens with callback function if lines are huge?
-# Handle unlink, rename and truncate
-# Handle signals
-# Handle unicode codifications
-# Watch for multiple files
-# Tests
-# Python 2 and 3 compatible
-# Publish to pypi
-# docstring Documentation?
+
+# TODO: Watch file using inotify
+# TODO: Apply callback function to every line as improvement
+# TODO: What happens with callback function if lines are huge?
+# TODO: Handle unlink, rename and truncate
+# TODO: Handle signals
+# TODO: Watch for multiple files
+# TODO: Add Tests, create mocks when necessary
+# TODO: Python 2 and 3 compatible
+# TODO: Publish to pypi providing bin and modules using generator
+# TODO: docstring Documentation?
+# TODO: Settup CI pipeline with travis
 
 import os
 import io
@@ -28,105 +30,52 @@ from time import sleep
 
 BLOCK_SIZE = io.DEFAULT_BUFFER_SIZE
 
-def tailf1(file, lines=10):
-    with open(file, 'r') as fd:
-        print([l for l in fd.read().splitlines() if l != ''][-1*lines:])
-        while True:
-            line = fd.readline()
-            if not line:
-                sleep(1)
-            else:
-                sys.stdout.write(line)
+def poll_file(fd):
+    fd.seek(0, os.SEEK_END)
+    while True:
+        new_line = fd.readline()
+        if not new_line:
+            sleep(0.1)
+        else:
+            sys.stdout.write(new_line)
 
-def tailf2(file, lines=10):
+
+def tailf(file, lines=10, callback=print):
     with open(file) as fd:
         fd.seek(0, os.SEEK_END)
         file_size = fd.tell()
-        print(file_size)
-        buffer = ''
+        buff = ''
 
         if file_size < BLOCK_SIZE:
             fd.seek(0, os.SEEK_SET)
-            buffer = fd.read()
+            buff = fd.read()
 
         else:
             counter = 1
-            while buffer.count(os.linesep) < lines +1:
+            while buff.count(os.linesep) < lines +1:
                 pointer = (-1 * counter) * BLOCK_SIZE
-                print('pointer: {0}'.format(pointer))
 
                 if abs(pointer) > file_size:
                     fd.seek(0, os.SEEK_SET)
-                    buffer = fd.read(file_size - (BLOCK_SIZE * (counter - 1))) + buffer
+                    buff = fd.read(file_size - (BLOCK_SIZE * (counter - 1))) + buff
                     break
 
                 else:
                     fd.seek(pointer, os.SEEK_END)
-                    buffer = fd.read(BLOCK_SIZE) + buffer
+                    buff = fd.read(BLOCK_SIZE) + buff
 
                 counter += 1
 
-        if buffer[-1:] == os.linesep:
-            buffer = buffer.rstrip()
+        if buff[-1:] == os.linesep:
+            buff = buff.rstrip()
 
-        tail_lines = buffer.split(os.linesep)[-1*lines:]
-        for line in tail_lines: print(line)
+        tail_lines = buff.split(os.linesep)[-1*lines:]
+        for line in tail_lines:
+            callback(line)
+
+        poll_file(fd)
 
 
-
-tailf = tailf2
-tailf('test3.txt')
-
-"""
-import os
-from abc import abstractmethod
-
-class Command(object):
-    def __init__(self, ops):
-        self.ops = ops
-
-    @abstractmethod
-    def run(self):
-        pass
-
-class Tail(Command):
-
-    nlines = 4
-    block_size = 2
-
-    def __init__(self, ops):
-        super(Tail, self).__init__(ops)
-        self.pointer = None
-        self.read_buffer = ""
-        self.lines_backward = []
-
-    def _move_pointer_backwards(self):
-        self.pointer = self.pointer - self.block_size
-        if not self.pointer > 0: self.pointer = 0
-        self.fd.seek(self.pointer)
-
-    def read_chunk(self):
-        self._move_pointer_backwards()
-        self.read_buffer += self.fd.read(self.block_size)
-        self._move_pointer_backwards() ## Because read move it forward by exactly block_size
-
-    def run(self):
-        with open('test.txt', 'r') as self.fd:
-            self.fd.seek(0, os.SEEK_END)
-            self.file_size = self.fd.tell()
-            self.pointer = self.fd.tell()
-
-            while self.fd.tell() > 0:
-                self.read_chunk()
-                chunk_lines = self.read_buffer.split("\n")
-                if chunk_lines > 2:
-                    self.lines_backward += chunk_lines[1:]
-                    self.pointer = self.pointer + len(chunk_lines[0])
-                    self.read_buffer = ""
-
-        print(self.lines_backward)
-
-Tail({}).run()
-"""
+tailf('test.txt')
 
 
